66c055a1b9cd7cebbea00d48df98515aad9941ba
aldhair8 <aldhairsin@gmail.com>
2020-10-14 15:28:10 +0000

    ejercicio 2

--- Exmanen_parcial/Ejercicio_2.py
+++ Exmanen_parcial/Ejercicio_2.py
@@ -1,21 +1,17 @@
 def AumentoaProfesoresAh():
-    #Datos de Entrada
-    salaMin=1247
-    puntos1Ah= int(input("Ingrese la cantidad de puntos:"))
+     #Datos de Entrada
+     salaMin= int(input("Dime tu salario: "))
+     puntos1Ah= int(input("Ingrese la cantidad de puntos:"))
 
-    if(puntos1Ah>=50 <=100):
-            salaMin+= salaMin+(salaMin*0.1)
-            print("el salarios es:", salaMin)
-    elif (puntos1Ah>=101 <=150):
-        salaMin2= salaMin+(salaMin*0.5)
-        print("el salarios es:", salaMin2)
-    else:
-        salaMin3= salaMin+(salaMin*0.8)
-        print("el salario es:", salaMin3)
-    
-            
-        
-    
-    
-
+     if(puntos1Ah>=50 and puntos1Ah <=100):
+         salaMin+= salaMin* 0.1
+         print("el salarios de 50:", salaMin)
+     elif (puntos1Ah>=101 and puntos1Ah <=150):
+         salaMin2= (salaMin)+(salaMin*0.5)
+         print("el salarios es 101 >:", salaMin2)
+     elif (puntos1Ah >= 151):
+         salaMin3= (salaMin)+(salaMin*0.8)
+         print("el salario es:", salaMin3)
+     else:
+         print("No hay salario para ti")
 AumentoaProfesoresAh()
